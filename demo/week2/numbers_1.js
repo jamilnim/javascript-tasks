092928b0a8871166232cb8219d7d151b3389e95d
jamilnim <jamilnimbook2@gmail.com>
2025-01-20 19:46:16 +0200

    add week 2 practice

---
+++ demo/week2/numbers_1.js
@@ -0,0 +1,15 @@
+/* 
+Create two variables:
+numString1 with the value "42".
+numString2 with the value "3.14".
+Use parseInt() to convert numString1 into an integer.
+Use parseFloat() to convert numString2 into a floating-point number.
+Print the results using console.log() with messages:
+"The integer is 42."
+"The floating-point number is 3.14."
+*/
+
+let numString1 = 42
+let numString2 = 3.14
+console.log("The integer is", parseInt(numString1))
+console.log(parseFloat(numString2))
---
+++ demo/week2/numbers_3.js
@@ -0,0 +1,20 @@
+/* 
+
+Create two variables:
+num1 with the value "10".
+num2 with the value "notANumber".
+Convert num1 and num2 using parseFloat().
+Print the sum of num1 and num2 using console.log().
+(Note: If one value is NaN, the result will be NaN, which will be printed automatically.)
+Example output:
+NaN (if the addition involves invalid input).
+10 (if both inputs are valid numbers).
+
+*/
+
+let num1 = "10"
+let num2 = "jam 25 euro"
+let num3 = parseFloat(num1)
+let num4 = parseFloat(num2)
+total=num3+num4
+console.log(total)
