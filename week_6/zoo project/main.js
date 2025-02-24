8e1db6ecb0da44ef94e293cf5392e4b5b43946ab
jamilnim <jamilnimbook2@gmail.com>
2025-02-25 00:32:34 +0200

    zoo project v1

--- week_6/zoo project/main.js
+++ week_6/zoo project/main.js
@@ -1,9 +1,36 @@
-const animalList2 = document.getElementById("animalList");
+const animalList = document.getElementById("animalList");
 const searchAnimal = document.getElementById("searchAnimal");
-const addAnimalName = ["Elephant", "Giraffe", "Panda", "Fox", "Kangaroo"];
+const newAnimalName = document.getElementById("newAnimalName");
+const addAnimalBtn = document.getElementById("addAnimal");
+const sortAnimalsBtn = document.getElementById("sortAnimals");
 
-function addanimal() {
-  animalList2.innerHTML = addAnimalName.john("<br>");
+const addAnimalNames = ["Elephant", "Giraffe", "Panda", "Fox", "Kangaroo"];
+
+function uploadName() {
+  animalList.innerHTML = "";
+  addAnimalNames.forEach((animalName) => {
+    const animalEliment = document.createElement("li");
+    animalEliment.innerText = animalName;
+    animalList.appendChild(animalEliment);
+  });
 }
 
-searchAnimal.addEventListener("change", addanimal);
+function addName() {
+  animalList.innerHTML = "";
+  addAnimalNames.push(newAnimalName.value);
+  addAnimalNames.forEach((animalName) => {
+    const animalEliment = document.createElement("li");
+    animalEliment.innerText = animalName;
+    animalList.appendChild(animalEliment);
+  });
+}
+
+function sortAnimal() {
+  animalList.innerHTML = "";
+  addAnimalNames.sort();
+  uploadName();
+}
+
+document.addEventListener("DOMContentLoaded", uploadName);
+addAnimalBtn.addEventListener("click", addName);
+sortAnimalsBtn.addEventListener("click", sortAnimal);
