68514d74f02e8ee5da1ed2606ac589df6fb27ad9
jamilnim <jamilnimbook2@gmail.com>
2025-03-07 09:03:58 +0200

    pancake vers 3 (self version)

--- week_7/pancake_mar 3rd phase/main.js
+++ week_7/pancake_mar 3rd phase/main.js
@@ -8,6 +8,7 @@ const customerName = document.getElementById("customerName");
 const summaryText = document.getElementById("summaryText");
 const orderId = Date.now();
 const orderIdDesplay = document.getElementById("orderID");
+const seeOrderBtn = document.querySelector("#seeOrder");
 
 const changeHandler = (event) => {
   // Oder id add
@@ -21,10 +22,6 @@ const changeHandler = (event) => {
   const toppingTotal = [
     ...document.querySelectorAll(".topping:checked"),
   ].reduce((sum, topping) => sum + parseFloat(topping.dataset.price), 0);
-
-  const toppingSelection = [
-    ...document.querySelectorAll(".topping:checked.vale"),
-  ];
 
   // extra selection with check box
   const extraTotal = [...document.querySelectorAll(".extra:checked")].reduce(
@@ -47,9 +44,41 @@ const changeHandler = (event) => {
   totalPriceBanner.textContent = `${
     basePrice + toppingTotal + extraTotal + deliveryPrice
   }€`;
-
-  //   summaryText.textContent = `Customer Name: ${customerName.value}
-  // your order is a ${pancakeType.value} toppings are ${toppingSelection}`;
 };
 
+const ordersObject = {
+  customrName: "",
+  orderID: "",
+  PancakeType: "",
+  toppings: [],
+  extras: [],
+  deliveryMethod: "",
+  TotalPrice: "",
+  status: "",
+};
+
+function orderSummary() {
+  ordersObject.customrName = customerName.value;
+  ordersObject.orderID = orderId;
+  ordersObject.PancakeType = pancakeType.value;
+  ordersObject.toppings = [];
+  const toppingsChecked = document.querySelectorAll(".topping:checked");
+  toppingsChecked.forEach((topping) =>
+    ordersObject.toppings.push(topping.value)
+  );
+  ordersObject.extras = [];
+  const extrasChecked = document.querySelectorAll(".extra:checked");
+  extrasChecked.forEach((extra) => ordersObject.extras.push(extra.value));
+
+  const deliveryMood = document.querySelector(".delivery:checked");
+  ordersObject.deliveryMethod = deliveryMood.value;
+
+  ordersObject.TotalPrice = totalPriceDisplay.textContent;
+
+  summaryText.innerHTML = `Customer name:${ordersObject.customrName} <br> Order ID:${ordersObject.orderID} <br> Pancake Type: ${ordersObject.PancakeType} <br>Toppings: ${ordersObject.toppings} <br>Extras: ${ordersObject.extras} <br>Mood of Delivery: ${ordersObject.deliveryMethod} <br>Total Price: ${ordersObject.TotalPrice} <br>Status: ${ordersObject.status}`;
+
+
+}
+
 pancakeForm.addEventListener("change", changeHandler);
+seeOrderBtn.addEventListener("click", orderSummary);
